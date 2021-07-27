bc8d37787280892cfa65c8fda20e032d366513ec
algml <algml9603@gmail.com>
2021-07-27 18:09:30 +0900

    createDB

--- back/routers/user/controller.js
+++ back/routers/user/controller.js
@@ -1,5 +1,13 @@
-
-let login =(req,res) =>{
+const {Board,User,Comment,Weather} = require('../../models')
+let login = async (req,res) =>{
+    let ress = await Weather.create({emo:'emo',text:'text'})
+    let resss = await Weather.findOne({
+        where:{
+            id:1
+        }
+    })
+    
+    
     res.send('login 입니다')
 }
 
--- back/server.js
+++ back/server.js
@@ -1,6 +1,23 @@
 const express = require('express')
 const app = express()
 const router = require('./routers/index')
+const {sequelize} = require('./models')
+
+sequelize.sync({force:true})
+.then(()=>{
+    console.log('db success')
+})
+.catch((err)=>{
+    console.log('db fail',err)
+})
+
+
+
+
+
+
+
+
 
 app.use('/',router)
 
